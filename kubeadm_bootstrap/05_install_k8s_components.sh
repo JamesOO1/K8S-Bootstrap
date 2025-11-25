d23dee23d2f9cc9a8d7f82d2c21c5ab98c41170d
James N <20262438+JamesOO1@users.noreply.github.com>
2025-11-25 06:13:18 -0600

    Update script to install Kubernetes components
    This script installs kubeadm, kubelet, and kubectl, updating the package index and adding the Kubernetes apt repository before installation.

---
+++ kubeadm_bootstrap/05_install_k8s_components.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# 05_install_k8s_components.sh
+#
+# Script to install kubeadm, kubelet, kubectl.
+# Returns 1 on failure
+# Process outlined here: https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#k8s-install-0
+# 
+# This script requires these commands:
+# apt-get, sudo
+
+# Step 1: "Update the apt package index and install packages needed to use the Kubernetes apt repository:"
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+# Step 2: "Download the public signing key for the Kubernetes package repositories. The same signing key is used for all repositories so you can disregard the version in the URL:"
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+# Step 3: "Add the appropriate Kubernetes apt repository. Please note that this repository have packages only for Kubernetes 1.34; for other Kubernetes minor versions, you need to change the Kubernetes minor version in the URL to match your desired minor version (you should also check that you are reading the documentation for the version of Kubernetes that you plan to install).
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# Step 4: "Update the apt package index, install kubelet, kubeadm and kubectl, and pin their version:"
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+# Step 5: " (Optional) Enable the kubelet service before running kubeadm:"
+sudo systemctl enable --now kubelet
