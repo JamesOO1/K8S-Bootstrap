4d78d2cb1c205a5d0894bdabbddc515c0c3c0093
James N <20262438+JamesOO1@users.noreply.github.com>
2025-11-26 04:05:23 -0600

    Fix typos in initiate_k8s_control_plane script

--- kubeadm_bootstrap/99_initiate_k8s_control_plane.sh
+++ kubeadm_bootstrap/99_initiate_k8s_control_plane.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# 100_iniate_k8s_control_plane.sh
+# 99_initiate_k8s_control_plane.sh
 #
-# Script to iniate the K8s control plane
+# Script to initiate the K8s control plane
 # Returns 1 if an error was encounterd
 # Returns 0 if successfully executed
 # Process outlined here: 
@@ -12,7 +12,7 @@
 POD_CIDR="10.244.0.0/16"
 SERVICE_CIDR="10.96.0.0/12"
 
-# 1. Iniate the K8S Cluster
+# 1. Initiate the K8S Cluster
 echo "------------------------------------------------------"
 echo "Initiate the K8S Cluster"
 echo "------------------------------------------------------"
